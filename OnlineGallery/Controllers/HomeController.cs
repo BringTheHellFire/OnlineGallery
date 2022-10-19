5163e8bcc83e3b621a7cb580bb61098dcbdcfac7
BringTheHellFire <73886873+BringTheHellFire@users.noreply.github.com>
2022-10-19 20:04:36 +0200

    Added function for getting next page of artworks from API

--- OnlineGallery/Controllers/HomeController.cs
+++ OnlineGallery/Controllers/HomeController.cs
@@ -18,7 +18,8 @@ namespace OnlineGallery.Controllers
         {
             APIArtwork artwork = await GetArtworkById();
             APIArtworkWithPagination artworkList = await GetAllArtworks();
-            return View(artworkList);
+            APIArtworkWithPagination artworkList2 = await GetNextPage(artworkList.Pagination.next_url);
+            return View(artworkList2);
         }
 
         public IActionResult Privacy()
@@ -57,6 +58,25 @@ namespace OnlineGallery.Controllers
         public async Task<APIArtworkWithPagination> GetAllArtworks()
         {
             var url = string.Format("https://api.artic.edu/api/v1/artworks");
+            var result = new APIArtworkWithPagination();
+            client = new HttpClient();
+            var response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var stringResponse = await response.Content.ReadAsStringAsync();
+
+                result = JsonSerializer.Deserialize<APIArtworkWithPagination>(stringResponse, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            }
+            else
+            {
+                throw new HttpRequestException(response.ReasonPhrase);
+            }
+
+            return result;
+        }
+
+        public async Task<APIArtworkWithPagination> GetNextPage(string url)
+        {
             var result = new APIArtworkWithPagination();
             client = new HttpClient();
             var response = await client.GetAsync(url);
