523bee4837aa1cf852ad28e735b94b57cdd51409
BringTheHellFire <73886873+BringTheHellFire@users.noreply.github.com>
2022-10-20 17:32:11 +0200

    Add controller for Gallery tab

---
+++ OnlineGallery/Controllers/APIArtworksController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineGallery.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace OnlineGallery.Controllers
+{
+    public class APIArtworksController : Controller
+    {
+        public async Task<IActionResult> IndexAsync()
+        {
+            APIArtwork artwork = await GetArtworkById();
+            APIArtworkWithPagination artworkList = await GetAllArtworks();
+            APIArtworkWithPagination artworkList2 = await GetNextPage(artworkList.Pagination.next_url);
+            return View(artworkList);
+        }
+        private HttpClient client;
+
+        public async Task<APIArtwork> GetArtworkById()
+        {
+            var url = string.Format("https://api.artic.edu/api/v1/artworks/129884?fields=title,image_id");
+            var result = new APIArtwork();
+            client = new HttpClient();
+            var response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var stringResponse = await response.Content.ReadAsStringAsync();
+
+                result = JsonSerializer.Deserialize<APIArtwork>(stringResponse, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            }
+            else
+            {
+                throw new HttpRequestException(response.ReasonPhrase);
+            }
+
+            return result;
+        }
+
+        public async Task<APIArtworkWithPagination> GetAllArtworks()
+        {
+            var url = string.Format("https://api.artic.edu/api/v1/artworks");
+            var result = new APIArtworkWithPagination();
+            client = new HttpClient();
+            var response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var stringResponse = await response.Content.ReadAsStringAsync();
+
+                result = JsonSerializer.Deserialize<APIArtworkWithPagination>(stringResponse, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            }
+            else
+            {
+                throw new HttpRequestException(response.ReasonPhrase);
+            }
+
+            return result;
+        }
+
+        public async Task<APIArtworkWithPagination> GetNextPage(string url)
+        {
+            var result = new APIArtworkWithPagination();
+            client = new HttpClient();
+            var response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var stringResponse = await response.Content.ReadAsStringAsync();
+
+                result = JsonSerializer.Deserialize<APIArtworkWithPagination>(stringResponse, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            }
+            else
+            {
+                throw new HttpRequestException(response.ReasonPhrase);
+            }
+
+            return result;
+        }
+    }
+}
--- OnlineGallery/Controllers/HomeController.cs
+++ OnlineGallery/Controllers/HomeController.cs
@@ -16,10 +16,7 @@ namespace OnlineGallery.Controllers
 
         public async Task<IActionResult> IndexAsync()
         {
-            APIArtwork artwork = await GetArtworkById();
-            APIArtworkWithPagination artworkList = await GetAllArtworks();
-            APIArtworkWithPagination artworkList2 = await GetNextPage(artworkList.Pagination.next_url);
-            return View(artworkList2);
+            return View();
         }
 
         public IActionResult Privacy()
@@ -31,67 +28,6 @@ namespace OnlineGallery.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        private HttpClient client;
-
-        public async Task<APIArtwork> GetArtworkById()
-        {
-            var url = string.Format("https://api.artic.edu/api/v1/artworks/129884?fields=title,image_id");
-            var result = new APIArtwork();
-            client = new HttpClient();
-            var response = await client.GetAsync(url);
-            if (response.IsSuccessStatusCode)
-            {
-                var stringResponse = await response.Content.ReadAsStringAsync();
-
-                result = JsonSerializer.Deserialize<APIArtwork>(stringResponse, new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase});
-            }
-            else
-            {
-                throw new HttpRequestException(response.ReasonPhrase);
-            }
-
-            return result;
-        }
-
-        public async Task<APIArtworkWithPagination> GetAllArtworks()
-        {
-            var url = string.Format("https://api.artic.edu/api/v1/artworks");
-            var result = new APIArtworkWithPagination();
-            client = new HttpClient();
-            var response = await client.GetAsync(url);
-            if (response.IsSuccessStatusCode)
-            {
-                var stringResponse = await response.Content.ReadAsStringAsync();
-
-                result = JsonSerializer.Deserialize<APIArtworkWithPagination>(stringResponse, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
-            }
-            else
-            {
-                throw new HttpRequestException(response.ReasonPhrase);
-            }
-
-            return result;
-        }
-
-        public async Task<APIArtworkWithPagination> GetNextPage(string url)
-        {
-            var result = new APIArtworkWithPagination();
-            client = new HttpClient();
-            var response = await client.GetAsync(url);
-            if (response.IsSuccessStatusCode)
-            {
-                var stringResponse = await response.Content.ReadAsStringAsync();
-
-                result = JsonSerializer.Deserialize<APIArtworkWithPagination>(stringResponse, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
-            }
-            else
-            {
-                throw new HttpRequestException(response.ReasonPhrase);
-            }
-
-            return result;
         }
 
     }
