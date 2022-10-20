0b68715a1d388a51ed20703e8431ad1125cd95ed
BringTheHellFire <73886873+BringTheHellFire@users.noreply.github.com>
2022-10-20 18:18:59 +0200

    Add controller and from for adding artworks

---
+++ OnlineGallery/Controllers/ArtworksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineGallery.Controllers
+{
+    public class ArtworksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+    }
+}
