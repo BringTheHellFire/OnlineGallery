068598a3f91aa1cdff4a4ddf9178e5d037958f35
BringTheHellFire <73886873+BringTheHellFire@users.noreply.github.com>
2022-10-26 18:56:03 +0200

    Add post edit function

--- OnlineGallery/Controllers/ArtworksController.cs
+++ OnlineGallery/Controllers/ArtworksController.cs
@@ -16,18 +16,22 @@ namespace OnlineGallery.Controllers
             _userManager = userManager;
         }
 
+
+
         public IActionResult Index()
         {
             var userArtworks = _dbContext.UserArtworks.ToList();
             return View(userArtworks);
         }
 
-        public IActionResult CreatePost()
+
+
+        public IActionResult Create()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult CreatePost(UserArtwork model)
+        public IActionResult Create(UserArtwork model)
         {
             Console.WriteLine(ModelState.IsValid);
             if (ModelState.IsValid)
@@ -46,6 +50,33 @@ namespace OnlineGallery.Controllers
                 this._dbContext.UserArtworks.Add(model);
                 this._dbContext.SaveChanges();
 
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+
+
+        [ActionName(nameof(Edit))]
+        public IActionResult Edit(int id)
+        {
+            var model = this._dbContext.UserArtworks.First(p => p.Id == id);
+            if (model.UserId == this._userManager.GetUserId(base.User))
+            {
+                return View(model);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        [ActionName(nameof(Edit))]
+        public async Task<IActionResult> EditPost(int id)
+        {
+            var post = this._dbContext.UserArtworks.Single(p => p.Id == id);
+            var ok = await this.TryUpdateModelAsync(post);
+
+            if (ok && this.ModelState.IsValid)
+            {
+                this._dbContext.SaveChanges();
                 return RedirectToAction("Index", "Home");
             }
             return View();
