e66488ddd7ae72c073f579656e7178814f680228
BringTheHellFire <73886873+BringTheHellFire@users.noreply.github.com>
2022-10-28 16:47:38 +0200

    Change look of APIArtwork Index page

--- OnlineGallery/Models/APIArtworkData.cs
+++ OnlineGallery/Models/APIArtworkData.cs
@@ -2,7 +2,9 @@
 {
     public class APIArtworkData
     {
+        public int id { get; set; }
         public string Title { get; set; }
         public string image_id { get; set; }
+        public string artist_title { get; set; }
     }
 }
